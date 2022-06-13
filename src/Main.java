a146f91a90f1bee811e0d545af92db19c8a4d35f
Tasilia <tsygnas@gmail.com>
2022-06-13 11:40:04 +0300

    Ver 2

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 public class Main {
+
     public static void main(String[] args) {
 
         int cost = 2022; // стоимость билета
         int price = 20; // кол-во рублей за одну милю
-        int mile = cost/price;
+        int mile = cost / price;
         System.out.println("Количество бонусных миль: " + mile);
 
     }
